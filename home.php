96941e3afe9a13d3af20cf8934d3f2e6368224fb
Rapaud Bastien <brapaud13008@gmail.com>
2023-02-02 01:35:13 +0100

    Copie Fix

--- home.php
+++ home.php
@@ -1,6 +1,16 @@
 <?php
 include 'navbar.php';
 
+$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// Requête pour récupérer toutes les données souhaitées
+$user = $_SESSION['email'];
+
+$query = 'SELECT password FROM password where user="'.$user.'"';
+// Exécution de la requête et stockage des résultats dans une variable
+$stmt = $bdd->prepare($query);
+$stmt->execute();
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 ?>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <table>
@@ -17,21 +27,57 @@ include 'navbar.php';
   $user = $_SESSION['email'];
   $stmt = $bdd->prepare('SELECT * FROM password where user="'.$user.'"');
   $stmt->execute();
+
   
   while ($passwordinfo = $stmt->fetch(PDO::FETCH_ASSOC)) {
-      echo('
+      echo'
   <tr>
     <td><a href="'.$passwordinfo['url'].'" target="_blank"><img src="'.$passwordinfo['photo'].'" alt="Image 1"></td>
     <td>'.$passwordinfo['email'].'</td>
-    <td><a href="#" ><i class="fa fa-eye"  style="font-size:28px;"></i></a></td>
+    ';?>
+    <td>
+      <div id="copy-buttons">
+        <a class="copy-button" data-value="<?php echo $passwordinfo['password']; ?>"><i class="fa fa-eye"  style="font-size:28px;"></i></a>
+</div>
+</td>   
+<?php
+echo' 
     <td><a href="editpassword.php?id='.$passwordinfo['id'].'"><i class="fa fa-pencil"  style="font-size:28px;"></i></a></td>
   </tr>
-  ');
+
+  ';
 }
 ?>
 
   
 </table>
+
+<script>
+    var copyButtons = document.querySelectorAll(".copy-button");
+    for (var i = 0; i < copyButtons.length; i++) {
+        copyButtons[i].addEventListener("click", function() {
+            // Récupération de la donnée depuis l'attribut "data-value"
+            var data = this.getAttribute("data-value");
+            // Création d'un élément de type "input" caché pour stocker temporairement la donnée
+            var tempInput = document.createElement("input");
+            // Attribution de la donnée à la valeur de l'élément "input"
+            tempInput.value = data;
+            // Ajout de l'élément à la page
+            document.body.appendChild(tempInput);
+            // Sélection de la valeur dans l'élément "input"
+            tempInput.select();
+            // Copie de la valeur sélectionnée dans le presse-papier
+            // Exécution de la commande "copy" pour copier la valeur dans le presse-papier
+            document.execCommand("copy");
+            // Suppression de l'élément "input" de la page
+            document.body.removeChild(tempInput);
+            // Notification pour indiquer que la copie a réussi
+        });
+    }
+</script>
+
+
+
 <style>
    table {
   width: calc(100% - 200px);
